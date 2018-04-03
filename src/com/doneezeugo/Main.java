1f134aa451be9a7551b05d468bebe1709ff4eb7b
deezeesms <deezeesms@gmail.com>
2018-04-03 14:16:12 -0400

    Testing from IntelliJ  version 3

--- src/com/doneezeugo/Main.java
+++ src/com/doneezeugo/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.println("from IDE-IntelliJ ... " + i);
+            System.out.println("from IDE-IntelliJ ... v3 " + i);
         }
 
     }

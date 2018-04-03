e629d1039f7838ec673edf2df74942edaadcc501
deezeesms <deezeesms@gmail.com>
2018-04-03 11:47:21 -0400

    Testing from IntelliJ  version 2

--- src/com/doneezeugo/Main.java
+++ src/com/doneezeugo/Main.java
@@ -3,10 +3,10 @@ package com.doneezeugo;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i < 25; i++) {
+        for (int i = 1; i < 30; i++) {
             System.out.println("from IDE-IntelliJ ... " + i);
         }
 
     }
-    
+
 }

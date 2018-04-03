d370ca95aeeb020cb3e7dd5b6b1af3343dad9b8b
deezeesms <deezeesms@gmail.com>
2018-04-03 11:05:15 -0400

    Initial commit from IntelliJ

---
+++ src/com/doneezeugo/Main.java
@@ -0,0 +1,12 @@
+package com.doneezeugo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 25; i++) {
+            System.out.println("from IDE-IntelliJ ... " + i);
+        }
+
+    }
+    
+}
